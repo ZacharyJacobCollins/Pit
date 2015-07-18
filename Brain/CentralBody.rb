9b600042f805db65d26af2858d24fe243892d091
shadey <me@shadey.net>
2015-07-17 22:33:56 -0400

    Fuck stanford and Java and Ruby

--- Brain/CentralBody.rb
+++
@@ -1,8 +0,0 @@
-require 'java'
-require_relative "../libs/stanford-corenlp-full-2015-04-20/stanford-corenlp-3.5.2.jar"
-class CentralBody
-  @responses = []
-  def appendResponse(x)
-    @responses << x
-  end
-end
--- Storyline/Storyline.rb
+++
@@ -1,4 +0,0 @@
-class Storyboard
-	@cycles = []
-	@instances = []
-end
