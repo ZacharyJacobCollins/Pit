cbd8e836b53711d870068c0f1f62fbbd14be5807
Zachary.Collins <ZacharyJacobCollins@gmail.com>
2015-07-19 00:12:06 -0400

    minus merge conflict shit

--- listener.py
+++ listener.py
@@ -9,22 +9,18 @@ def GetMessages():
         if len(messageList) < 5:
             messageList.append(message['text'])
         else:
-            break  
-        
+            break
+
     return messageList
-    
+
 def UpdateChannelName(update):
     urllib2.urlopen("https://slack.com/api/channels.rename?token=xoxp-7542066518-7542253777-7877737223-5f8bd0&channel=C07RQLZDG&name={0}&pretty=1".format(update))
     sleep(2)
     messageJson = GetJson(("https://slack.com/api/channels.history?token=xoxp-7542066518-7542253777-7877737223-5f8bd0&channel=C07RQLZDG&pretty=1"))
     timeStamp = messageJson['messages'][0]['ts']
-<<<<<<< Updated upstream
-    #urllib2.urlopen("https://slack.com/api/chat.delete?token=xoxp-7542066518-7542253777-7877737223-5f8bd0&ts={0}&channel=C07RQLZDG&pretty=1".format(timeStamp))
-=======
     urllib2.urlopen("https://slack.com/api/chat.delete?token=xoxp-7542066518-7542253777-7877737223-5f8bd0&ts={0}&channel=C07RQLZDG&pretty=1".format(timeStamp))
-   
+
 def GetJson(uri):
     messageText = urllib2.urlopen(uri).read()
     messageJson = json.loads(messageText)
-    return messageJson    
->>>>>>> Stashed changes
+    return messageJson
