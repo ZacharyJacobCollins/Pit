25a86f2d7f723ecaff8941155568e866ecea4093
Sam R (Shadey) <srobinson1115@starkstate.net>
2015-07-20 19:48:56 -0400

    MORE GARBAGE

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from sentence import Sentence
-<<<<<<< HEAD
 from tagger import findSubject
 from time import sleep
 import listener
@@ -8,19 +7,16 @@ def main():
     while True:
         timeStamp = listener.DeleteNotification()
         messages = listener.GetMessages(timeStamp)
-=======
 from algo import findSubject
 from time import sleep
 import listener
 def main():
     while True:
         messages = listener.GetMessages()
->>>>>>> fa5373ce13370b00d277006aea28d982e1ee2095
         print findSubject(messages)
         if findSubject(messages) != None:
             print "Changing channel"
             listener.UpdateChannelName(findSubject(messages))
-<<<<<<< HEAD
         sleep(0)
 if __name__ == '__main__':
     main()
